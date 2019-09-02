bfeece421f4a87e9900a04d76359b7bb2b215d6d
Isra Ragheb <israragheb@gmail.com>
2019-09-02 18:00:17 +0200

    master

---
+++ App.js
@@ -0,0 +1,77 @@
+var express=require('express');
+var parser=require('body-parser');
+const formidable=require('formidable');
+var path = require('path');
+var fs=require('fs');
+app=express();
+var filecounter=0;
+
+//creating a public directory to access specfic resourses
+var publicDir=require('path').join(__dirname,'/Public');
+app.use(express.static(publicDir));
+
+
+//set the view engine to be ejs
+app.set('view engine', 'ejs');
+
+// home page route
+app.get('/',function(req,res){
+    res.render('UploadPage/uploadpage',{messageexec:'',messagelogo:'',logopath:'',VendorName:''});
+});
+
+//upload page route
+app.post('/add',function(req,res){
+   var form=new formidable();
+   
+  filecounter++;
+   form.parse(req,function(err,fields,files){
+       var Name=fields.NameInput;
+       var VendorName=fields.VendorName;
+   });
+
+   form.on('fileBegin',function(name,file){
+    if(name ==='execfilepath'){
+        fs.rename(file.name,filecounter+'.exec',function(err){
+            if (err) throw err;
+            console.log('File Remamed!')
+        })
+        file.path=  __dirname + '/Applications/' + filecounter+'.exec';
+    }
+    if(name ==='logofilepath' && !(name ==='execfilepath')){
+        file.path=  __dirname + '/Public/ImagesUploaded/' + filecounter+file.name;
+    }
+   });
+
+   res.render('UploadPage/successpage');
+});     
+
+//home page route
+app.get('/home',function(req,res){
+    fs.readdir(publicDir+'/ImagesUploaded',function(err,files){
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } 
+        res.render('Homepage/homepage.ejs',{fileImageArray:files});
+
+    });
+    
+});
+
+//download route
+app.post('/download/:todownloadurl',function(req,res){
+    fs.readdir(__dirname+'/Applications',function(err,fileApp){
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }  
+        var filePath=__dirname+'/Applications/'+fileApp[req.params.todownloadurl];
+        res.download(filePath);
+    });
+   
+   
+});
+
+app.listen(3000);
+
+
+
+
